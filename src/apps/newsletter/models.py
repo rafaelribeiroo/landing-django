96404156914762b1453280d8edfbfe8d8804c90d
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-01-06 17:57:39 -0200

    Novas funcionalidades

--- src/apps/newsletter/models.py
+++ src/apps/newsletter/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class dadosHome(models.Model):
+    imagem = models.ImageField(
+    	upload_to='landing_images',
+    	#default='media/default.png'
+    	)
+    titulo_principal = models.CharField(
+        'Título Principal',
+        max_length=80,
+        null=True,
+        unique=True)
+    subtitulo = models.CharField(
+        'Subtítulo',
+        max_length=80,
+        null=False)
+    texto = models.CharField(
+        'Inclua o texto',
+        max_length=1500,
+        null=False,
+        blank=False)
+
+    class Meta:
+        ordering = ['titulo_principal']
+        verbose_name = 'Informação da página principal'
+        verbose_name_plural = 'Dados da página principal'
+
+    def __str__(self):
+        return self.titulo_principal
